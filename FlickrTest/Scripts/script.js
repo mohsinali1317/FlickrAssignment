ba70e437fd47d86a32145174698f4d6df66455a5
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2015-10-07 11:18:49 +0200

    Added feature for selecting between cacheable or normal flickr repo

--- FlickrTest/Scripts/script.js
+++ FlickrTest/Scripts/script.js
@@ -9,34 +9,29 @@ $("#tags").keyup(function () {
         if ($("#tags").val().length < 3) {
             var container = $("#container");
             container.html("");
+            $("#link").html("");
             return false;
         }
 
-        var URL = window.location + "/Home/GetImages?tags=" + $("#tags").val();
+        var flickerOption = $("#flickerType").val();
+
+        var cacheFlicker = (flickerOption == "cachedFlicker") ? true : false;
+
+        var URL = window.location + "/Home/GetImages";
 
         $("#link").html(URL).attr("href", URL);
 
         $(".spinner-loader").show();
 
 
-        $.post(URL, function (data) {
+        $.post(URL, { tags: $("#tags").val(), cacheFlicker: cacheFlicker }, function (data) {
             var container = $("#container");
             container.html("");
-
-         //   var mainRow = $("<div>");
-
-
-           // mainRow.addClass("row");
 
             var ul = $("<ul>");
             ul.addClass("clearing-thumbs small-block-grid-1 medium-block-grid-3 large-block-grid-5");
 
             $.each(data, function () {
-
-                
-
-              //  var thumb = $("<div>");
-                // thumb.addClass("medium-4 columns img-contianer");
 
                 var li = $("<li>");
                 li.addClass("item");
