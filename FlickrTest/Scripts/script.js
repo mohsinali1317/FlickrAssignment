150b607113b602e63bfc89b9d5c8c96f17d5b8eb
Chaudhry Mohsin Ali <mohsinali1017@gmail.com>
2015-10-08 13:04:56 +0200

    done with the design bits

--- FlickrTest/Scripts/script.js
+++ FlickrTest/Scripts/script.js
@@ -1,7 +1,8 @@
 ﻿
 var tags = null;
+var imageArray = [];
 $("#tags").keyup(function () {
-    
+
 
     clearTimeout(tags);
     tags = setTimeout(function () {
@@ -27,36 +28,70 @@ $("#tags").keyup(function () {
 
 
         $.post(URL, { tags: tags, cacheFlicker: cacheFlicker }, function (data) {
-            var container = $("#container");
-            container.html("");
-
-            var ul = $("<ul>");
-            ul.addClass("clearing-thumbs small-block-grid-1 medium-block-grid-3 large-block-grid-5");
-
-            $.each(data, function () {
-
-                var li = $("<li>");
-                li.addClass("item");
-
-                var image = $("<img>")
-                    .attr("src", this.ImageUrl);
-
-                image.addClass("item");
-
-                var title = $("<span>")
-                    .html(this.Title);
-
-                li.append(title);
-                li.append(image);
-
-                ul.append(li);
-
-                container.append(ul);
-            });
-
+            imageArray = [];
+            storeInImageArray(data);
+            loadImages();
             $(".spinner-loader").hide();
-
         });
 
     }, 250);
 })
+
+function storeInImageArray(data) {
+
+    var container = $("#container");
+    container.html("");
+
+    var ul = $("<ul>");
+    ul.addClass("clearing-thumbs small-block-grid-1 medium-block-grid-3 large-block-grid-5");
+    var count = 0;
+    $.each(data, function () {
+        imageArray.push(this.ImageUrl);
+
+        var li = $("<li>");
+        li.addClass("item");
+
+        var image = $("<img>")
+                    .addClass("image-item");
+
+        var title = $("<span>")
+                    .html(this.Title);
+
+        var source = $("<span>")
+                    .addClass("source image-item" + "_" + count++);
+
+        // li.append(title);
+        li.append(source);
+        li.append(image);
+
+        ul.append(li);
+
+        container.append(ul);
+    });
+}
+
+function loadImages() {
+
+    $('.image-item').each(function (index, elem) {
+        elem.src = imageArray[index];
+        if (!elem.complete) {
+            $(elem).load(function () {
+                rescaleImage(elem);
+                $(".image-item_" + index).html("Loaded From Flicker");
+            });
+        }
+        else {
+            rescaleImage(elem);
+
+            $(".image-item_" + index).html("Loaded From Cache");
+        }
+    });
+}
+
+function rescaleImage(elem) {
+    var logoHeight = $(elem).height();
+    if (logoHeight < 222) {
+        var margintop = (222 - logoHeight) / 2;
+        $(elem).css('margin-top', margintop);
+    }
+}
